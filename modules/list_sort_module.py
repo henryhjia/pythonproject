2e1fdb57cbbbe7b9b8c0a19a740a959f40a118d5
henry jia <henryhjia@gmail.com>
2023-09-22 16:18:58 -0500

    update list sort module

--- modules/list_sort_module.py
+++ modules/list_sort_module.py
@@ -130,5 +130,36 @@ class SortListTester:
     sorted_list = sorted(my_list_of_dict, key=lambda d: list(d.keys())[0])     
     print('sort by key:    ', sorted_list)
 
+  def sort_list_of_list(self) -> None:
+    """
+    1. Sort list of list
+    """
+    common_module.print_function(self.sort_list_of_list) 
+    
+    my_list = [
+      [1,2,3],
+      [3,2,1],
+      [2,5,5],
+      [0,1,4]
+    ]
+
+    for i in my_list:
+      print(i)
+
+    sorted_list = sorted(my_list, key=lambda x:x[0])
+    print('sort based on x[0]')
+    for i in sorted_list:
+      print(i)
+
+    sorted_list = sorted(my_list, key=lambda x:x[1])
+    print('sort based on x[1]')
+    for i in sorted_list:
+      print(i)
+
+    sorted_list = sorted(my_list, key=lambda x:x[2])
+    print('sort based on x[2]')
+    for i in sorted_list:
+      print(i)
+
 if __name__ == '__main__':
     print('module only, not main')
