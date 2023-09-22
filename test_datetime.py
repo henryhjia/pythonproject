c3a23706957400a750e3c338dac1c532c54b8af9
henry jia <henryhjia@gmail.com>
2023-09-22 18:00:10 -0500

    rename datetime file

---
+++ test_datetime.py
@@ -0,0 +1,15 @@
+#!/use/bin/python3
+"""
+date time tester 
+@datetime
+"""
+import datetime_module
+
+def main() -> int:
+
+  me = datetime_module.Datetime_Tester()
+
+  me.test_datetime()
+
+if __name__ == '__main__':
+  main()
