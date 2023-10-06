1383e3665945dd497503ee80e957f4778b32f80c
henry jia <henryhjia@gmail.com>
2023-10-05 19:45:29 -0500

    add class variable

---
+++ test_class_variable.py
@@ -0,0 +1,33 @@
+"""
+test class variable and instance variable
+@classvariable
+@instancevariable
+"""
+
+class User:
+  class_var1 = 1
+
+  def __init__(self, name, email, count):
+    self._name = name
+    self._email = email
+    self._count = count
+
+  def get_name(self):
+    return self._name
+
+  def get_email(self):
+    return self._email
+
+  def do_something(self):
+    print('Instance variable from ' + self._name + ' is: ' + str(self._count))
+
+  def __str__(self):
+    return self._name + ', ', + self._email
+
+def main():
+  users = [ User('david', 'd@gmail.com', 1), User('katie','k@gmail.com',2 )]
+  for user in users:
+    user.do_something()
+    print('Class variable=', User.class_var1)
+
+main()
