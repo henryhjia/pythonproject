83b52094c36f708dd1a833ee765b25e33ebff371
henry jia <henryhjia@gmail.com>
2024-11-14 07:30:49 -0600

    add daterange

--- clean_disk_files.py
+++ clean_disk_files.py
@@ -1,79 +1,84 @@
 """
 clearn files on disk
 """
+
 import json
 import argparse
 from datetime import datetime, timedelta, timezone
 from pathlib import Path
 from collections import defaultdict
 
+
 class CleanDiskFiles:
-  JSON_KEY_TO_FIND = 'setLaunchDate'
-  DAYS_DELTA = 14
+    JSON_KEY_TO_FIND = "setLaunchDate"
+    DAYS_DELTA = 14
 
-  def __init__(self, file_dir):
-    self.json_dir = file_dir
+    def __init__(self, file_dir):
+        self.json_dir = file_dir
 
-  def compare_datetime(self, index, directory_path, json_file, json_data):
-    """
-    find setLaunchDate and compare it with current datetime. if it is DAYS_DELTA old, delete it from OS
-    """
-    pattern = json_file.name.rsplit('.', 2)[0] + '*'
-    if CleanDiskFiles.JSON_KEY_TO_FIND in json_data:
-      date_str = json_data[CleanDiskFiles.JSON_KEY_TO_FIND]
-      json_date_tz = datetime.fromisoformat(date_str)
-      current_time = datetime.now(timezone.utc)
-      time_diff = current_time - json_date_tz
-      if time_diff > timedelta(days=CleanDiskFiles.DAYS_DELTA):
-        for file_path in directory_path.glob(pattern):
-          print(f'{index} {json_file.name} {pattern} {json_date_tz} {current_time} > {CleanDiskFiles.DAYS_DELTA} days, REMOVE')
-          file_path.unlink()
-      else:
-        print(f'{index} {json_file.name} {pattern} {json_date_tz} {current_time} < {CleanDiskFiles.DAYS_DELTA} days, KEEP')
+    def compare_datetime(self, index, directory_path, json_file, json_data):
+        """
+        find setLaunchDate and compare it with current datetime. if it is DAYS_DELTA old, delete it from OS
+        """
+        pattern = json_file.name.rsplit(".", 2)[0] + "*"
+        if CleanDiskFiles.JSON_KEY_TO_FIND in json_data:
+            date_str = json_data[CleanDiskFiles.JSON_KEY_TO_FIND]
+            json_date_tz = datetime.fromisoformat(date_str)
+            current_time = datetime.now(timezone.utc)
+            time_diff = current_time - json_date_tz
+            if time_diff > timedelta(days=CleanDiskFiles.DAYS_DELTA):
+                for file_path in directory_path.glob(pattern):
+                    print(
+                        f"{index} {json_file.name} {pattern} {json_date_tz} {current_time} > {CleanDiskFiles.DAYS_DELTA} days, REMOVE"
+                    )
+                    file_path.unlink()
+            else:
+                print(
+                    f"{index} {json_file.name} {pattern} {json_date_tz} {current_time} < {CleanDiskFiles.DAYS_DELTA} days, KEEP"
+                )
 
-  def read_remove_json_file(self):
-    """
-    read json file one at the time, retrieve json content
-    """
-    directory_path = Path(self.json_dir)
-    for i, json_file in enumerate(directory_path.glob('*.json')):
-      with open(json_file) as file:
-        json_data = json.load(file)
-        self.compare_datetime(i, directory_path, json_file, json_data)
+    def read_remove_json_file(self):
+        """
+        read json file one at the time, retrieve json content
+        """
+        directory_path = Path(self.json_dir)
+        for i, json_file in enumerate(directory_path.glob("*.json")):
+            with open(json_file) as file:
+                json_data = json.load(file)
+                self.compare_datetime(i, directory_path, json_file, json_data)
 
-  def purge_other_files(self, filename):
-    """
-    remove older files based on the date in the filename
-    """
-    directory_path = Path(self.json_dir)
-    date_file_dict = defaultdict(list)
-    latest_date = None
-    for i, to_be_deleted_file in enumerate(directory_path.glob(filename)):
-      date_str = to_be_deleted_file.name.split('.')[0].split('_')[-1]
-      this_date = datetime.strptime(date_str, "%Y-%m-%d")
-      date_file_dict[this_date].append(to_be_deleted_file)
-      if latest_date is None or this_date >= latest_date:
-        latest_date = this_date
+    def purge_other_files(self, filename):
+        """
+        remove older files based on the date in the filename
+        """
+        directory_path = Path(self.json_dir)
+        date_file_dict = defaultdict(list)
+        latest_date = None
+        for i, to_be_deleted_file in enumerate(directory_path.glob(filename)):
+            date_str = to_be_deleted_file.name.split(".")[0].split("_")[-1]
+            this_date = datetime.strptime(date_str, "%Y-%m-%d")
+            date_file_dict[this_date].append(to_be_deleted_file)
+            if latest_date is None or this_date >= latest_date:
+                latest_date = this_date
 
-    for key, value in date_file_dict.items():
-      if key != latest_date:
-        for item in value:
-          item.unlink()
-          print(f'{key} {item} deleted')
-      else:
-        for item in value:
-          print(f'{key} {item} kept')
+        for key, value in date_file_dict.items():
+            if key != latest_date:
+                for item in value:
+                    item.unlink()
+                    print(f"{key} {item} deleted")
+            else:
+                for item in value:
+                    print(f"{key} {item} kept")
 
-if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
-  parser.add_argument(
-    'directory', type=Path, help='help text')
-  args = parser.parse_args()
 
-  my_obj = CleanDiskFiles(args.directory)
-  my_obj.read_remove_json_file()
-  my_obj.purge_other_files('aaa*.csv')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("directory", type=Path, help="help text")
+    args = parser.parse_args()
 
-  
-      
-print('done')
+    my_obj = CleanDiskFiles(args.directory)
+    my_obj.read_remove_json_file()
+    my_obj.purge_other_files("aaa*.csv")
+
+
+print("done")
---
+++ daterange.py
@@ -0,0 +1,36 @@
+"""
+calculate date range 
+end_date = today or specific day
+start_date = end_date - delta
+"""
+
+import json
+import argparse
+from datetime import datetime, timedelta, timezone
+from pathlib import Path
+from collections import defaultdict
+
+
+class DateRange:
+
+    DAYS_DELTA = 14
+
+    def __init__(self, file_dir):
+        self.json_dir = file_dir
+
+    def get_date_range(self, index, directory_path, json_file, json_data) -> tuple[datetime, datetime]:
+        begin_date = None
+        end_date = None
+        return begin_date, end_date
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("directory", type=Path, help="help text")
+    args = parser.parse_args()
+
+    my_obj = DateRange(args.directory)
+    my_obj.get_date_range()
+
+
+print("done")
