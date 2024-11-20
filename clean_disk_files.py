083edf7576ac393b7ff5016f400ea3870ca75267
henry jia <henryhjia@gmail.com>
2024-11-20 10:37:16 -0600

    Update clear file

--- clean_disk_files.py
+++ clean_disk_files.py
@@ -1,5 +1,50 @@
 """
-clearn files on disk
+Clearn files on disk
+
+CASE 1:
+Read datetime from json "setLaunchDate", 
+if it is less than 2 weeks from provided last_date, keep it
+if it is older than 2 weeks from provided last_date, delete it (as well as any
+    other files with the same pattern, e.g. cm2*)
+
+pattern: if file is cm2.csv.json, the patten is cm2*.  
+
+input files:
+cm1.csv.json:
+
+{"a": 1, "b":2, "setLaunchDate": "2024-11-06T12:57:39+00:00"}
+
+cm1.csv
+- a csv file
+
+
+cm2.xml.json
+{"a": 1, "b":2, "setLaunchDate": "2024-11-12T12:57:39+00:00"}
+
+cm2.xml
+ - a xml file
+
+CASE 2: 
+-rw-rw-r-- 1 henryjia henryjia  8 Nov 20 10:24 aaa1_2024-10-10.csv
+-rw-rw-r-- 1 henryjia henryjia 10 Nov 20 10:24 aaa1_2024-11-02.csv
+-rw-rw-r-- 1 henryjia henryjia 11 Nov 20 10:24 aaa1_2024-11-08.csv
+-rw-rw-r-- 1 henryjia henryjia 11 Nov 20 10:24 aaa2_2024-11-01.csv
+-rw-rw-r-- 1 henryjia henryjia 11 Nov 20 10:24 aaa2_2024-11-08.csv
+-rw-rw-r-- 1 henryjia henryjia 11 Nov 20 10:24 aaa3_2024-10-30.csv
+-rw-rw-r-- 1 henryjia henryjia 11 Nov 20 10:24 aaa3_2024-11-08.csv
+
+It will keep the file with latest datetime in the file name.  In this case,
+these are kept:
+
+aaa1_2024-11-08.csv
+aaa2_2024-11-08.csv
+aaa3_2024-11-08.csv
+
+Rest files are deleted.
+File contents can be anything
+
+All data are backed up in ../data/
+
 """
 
 import json
@@ -13,8 +58,9 @@ class CleanDiskFiles:
     JSON_KEY_TO_FIND = "setLaunchDate"
     DAYS_DELTA = 14
 
-    def __init__(self, file_dir):
+    def __init__(self, file_dir:str, last_date:str):
         self.json_dir = file_dir
+        self.last_date = last_date
 
     def compare_datetime(self, index, directory_path, json_file, json_data):
         """
@@ -24,17 +70,17 @@ class CleanDiskFiles:
         if CleanDiskFiles.JSON_KEY_TO_FIND in json_data:
             date_str = json_data[CleanDiskFiles.JSON_KEY_TO_FIND]
             json_date_tz = datetime.fromisoformat(date_str)
-            current_time = datetime.now(timezone.utc)
+            current_time = datetime.fromisoformat(self.last_date).replace(tzinfo=timezone.utc)
             time_diff = current_time - json_date_tz
             if time_diff > timedelta(days=CleanDiskFiles.DAYS_DELTA):
                 for file_path in directory_path.glob(pattern):
                     print(
-                        f"{index} {json_file.name} {pattern} {json_date_tz} {current_time} > {CleanDiskFiles.DAYS_DELTA} days, REMOVE"
+                        f"{index} {json_file.name} pattern={pattern} {json_date_tz} {current_time} > {CleanDiskFiles.DAYS_DELTA} days, REMOVE"
                     )
                     file_path.unlink()
             else:
                 print(
-                    f"{index} {json_file.name} {pattern} {json_date_tz} {current_time} < {CleanDiskFiles.DAYS_DELTA} days, KEEP"
+                    f"{index} {json_file.name} pattern={pattern} {json_date_tz} {current_time} < {CleanDiskFiles.DAYS_DELTA} days, KEEP"
                 )
 
     def read_remove_json_file(self):
@@ -73,10 +119,11 @@ class CleanDiskFiles:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("directory", type=Path, help="help text")
+    parser.add_argument("directory", type=Path, help="directory where data locates")
+    parser.add_argument("last_date", type=str, help="Date of last report e.g. '2024-11-15'")
     args = parser.parse_args()
 
-    my_obj = CleanDiskFiles(args.directory)
+    my_obj = CleanDiskFiles(args.directory, args.last_date)
     my_obj.read_remove_json_file()
     my_obj.purge_other_files("aaa*.csv")
 
--- daterange.py
+++ daterange.py
@@ -9,7 +9,7 @@ import argparse
 from datetime import datetime, timedelta, timezone
 from pathlib import Path
 from collections import defaultdict
-
+from typing import Union
 
 class DateRange:
 
@@ -18,7 +18,7 @@ class DateRange:
     def __init__(self, file_dir):
         self.json_dir = file_dir
 
-    def get_date_range(self, index, directory_path, json_file, json_data) -> tuple[datetime, datetime]:
+    def get_date_range(self, begin_days=14, start_weekday: Union[str,int] = None, max_day=None) -> tuple[datetime, datetime]:
         begin_date = None
         end_date = None
         return begin_date, end_date
@@ -26,11 +26,11 @@ class DateRange:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("directory", type=Path, help="help text")
+    # parser.add_argument("directory", type=Path, help="help text")
     args = parser.parse_args()
+    print(f'args={args}')
+    
+    my_obj = DateRange(args)
+    begin, end = my_obj.get_date_range()
+    print(f'begin={begin} end={end}')
 
-    my_obj = DateRange(args.directory)
-    my_obj.get_date_range()
-
-
-print("done")
