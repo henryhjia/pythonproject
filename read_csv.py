1b55c35dba4095282b07899292783a312823e28a
henry jia <henryhjia@gmail.com>
2025-09-15 21:53:58 -0500

    add csv, json reader

--- read_csv.py
+++ read_csv.py
@@ -3,11 +3,11 @@
 read csv file 
 @readcsv
 """
-import csv_reader_module
+from modules import csv_reader_module
 
 def main() -> None:
 
-  me = csv_reader_module.CsvReader('../data/mpg.csv')
+  me = csv_reader_module.CsvReader('data/mpg.csv')
   me.read_csv()
 
 if __name__ == '__main__':
