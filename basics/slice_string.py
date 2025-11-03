2ae1106a03a6c67c77cbf10ce7db2a966b6fdd72
henry jia <henryhjia@gmail.com>
2025-11-03 13:26:17 -0600

    update Dockerfile

--- basics/slice_string.py
+++ basics/slice_string.py
@@ -15,9 +15,12 @@ from modules import string_slicing_module
 def main() -> None:
   """
   """
+  print("Wlcome to string slicing example!")
+
   # Determine directory where the current script lives
   log_dir = os.path.dirname(__file__)
   log_file = os.path.join(log_dir, 'slice_string.log')
+  print(f'Log file will be created at: {log_file}')
 
   # Configure logging
   logging.basicConfig(filename=log_file, level=logging.INFO, filemode='w')
@@ -30,13 +33,19 @@ def main() -> None:
 
   logging.info(f'original string= {in_str}')
   logging.info(f'pattern= {pattern}')
+  print(f'original string= {in_str}')
+  print(f'pattern= {pattern}')
 
   result = me.find_string_using_looping(in_str, pattern)
   logging.info(f'The total number of matching string is {result}')
+  print(f'The total number of matching string is {result}')
 
   result = me.find_string_using_slicing(in_str, pattern)
   logging.info(f'The total number of matching string \'{pattern}\' is {result}')
+  print(f'The total number of matching string \'{pattern}\' is {result}')
   logging.shutdown()
+
+  print(f'Log file created at: {log_file}')
 
 if __name__ == '__main__':
   main()
